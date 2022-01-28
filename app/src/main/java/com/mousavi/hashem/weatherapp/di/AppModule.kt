07d80c10b59aa9575151bbeb613f5486f1f68b85
Hashem <Hashem.Mousavi.m@gmail.com>
2022-01-28 21:47:06 +0330

    add di skeleton

---
+++ app/src/main/java/com/mousavi/hashem/weatherapp/di/AppModule.kt
@@ -0,0 +1,11 @@
+package com.mousavi.hashem.weatherapp.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+
+@Module
+@InstallIn(ViewModelComponent::class)
+object AppModule {
+
+}
